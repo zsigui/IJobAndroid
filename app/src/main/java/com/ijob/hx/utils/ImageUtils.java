db1cafaec20f0e06542998630b02531f3239ed49
zsigui <zsigui@sina.com>
2015-03-10 08:50:41 +0800

    继续修改

---
+++ app/src/main/java/com/ijob/hx/utils/ImageUtils.java
@@ -0,0 +1,26 @@
+package com.ijob.hx.utils;
+
+import com.easemob.util.EMLog;
+import com.easemob.util.PathUtil;
+
+/**
+ * Created by JackieZhuang on 2015/3/10.
+ */
+public class ImageUtils {
+	public static String getImagePath(String remoteUrl)
+	{
+		String imageName= remoteUrl.substring(remoteUrl.lastIndexOf("/") + 1, remoteUrl.length());
+		String path = PathUtil.getInstance().getImagePath()+"/"+ imageName;
+		EMLog.d("msg", "image path:" + path);
+		return path;
+
+	}
+
+
+	public static String getThumbnailImagePath(String thumbRemoteUrl) {
+		String thumbImageName= thumbRemoteUrl.substring(thumbRemoteUrl.lastIndexOf("/") + 1, thumbRemoteUrl.length());
+		String path =PathUtil.getInstance().getImagePath()+"/"+ "th"+thumbImageName;
+		EMLog.d("msg", "thum image path:" + path);
+		return path;
+	}
+}
